d2c5c6b83e5bd98cdca7c7e7b7445876a0ffa658
yenle73 <97496482+kimyen07@users.noreply.github.com>
2023-06-06 22:31:42 +0700

    pages name

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ import streamlit as st
 st.markdown("<h1 style='text-align: center; color: #10316B;'>Movie Recommendation System</h1>", unsafe_allow_html=True)
 
 show_pages([
-    Page("main.py", "Home Page", ":house:"),
-    Page("pages/page1.py", "Content Based Recommender", ":notebook:"),
-    Page("pages/page2.py", "Mood and Genre Based Recommender", ":blue_book:"),
-    Page("pages/page3.py", "Collaborative Recommender", ":bar_chart:"),
+    Page("pages/page1.py", "Pixel-wise Comparison", ":notebook:"),
+    Page("pages/page2.py", "Edge Detection Comparison", ":blue_book:"),
+    Page("pages/page3.py", "Structural Similarity Index Comparison", ":bar_chart:"),
 ])
