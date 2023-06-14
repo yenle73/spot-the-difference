fb71a6c243cf23d8a6d0e9001fa365b003e95ae3
yenle73 <97496482+kimyen07@users.noreply.github.com>
2023-06-14 12:51:55 +0700

    title

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from st_pages import Page, Section, add_page_title, show_pages
 import streamlit as st
 
-st.markdown("<h1 style='text-align: center; color: #10316B;'>Movie Recommendation System</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: #10316B;'>Spot the Differences</h1>", unsafe_allow_html=True)
 
 show_pages([
     Page("pages/page1.py", "Pixel-wise Comparison", ":notebook:"),
